d1c79404cdb2d2e940b8411ca4416594467b030b
Dragon <dragonofmercy@gmail.com>
2026-04-17 17:44:21 +0200

    feat: extend Trailer with optional Info and ID

--- src/Writer/Trailer.php
+++ src/Writer/Trailer.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace PhpPdf\Writer;
 
 use PhpPdf\Writer\Object\Dictionary;
+use PhpPdf\Writer\Object\HexString;
 use PhpPdf\Writer\Object\Name;
+use PhpPdf\Writer\Object\PdfArray;
 use PhpPdf\Writer\Object\PdfNumber;
 use PhpPdf\Writer\Object\PdfReference;
 
@@ -20,6 +22,8 @@ final readonly class Trailer
         private int $size,
         private PdfReference $root,
         private int $xrefOffset,
+        private ?PdfReference $info = null,
+        private ?string $documentId = null,
     ) {}
 
     public function toBytes(): string
@@ -27,6 +31,16 @@ final readonly class Trailer
         $dict = Dictionary::empty()
             ->withEntry(Name::of('Size'), PdfNumber::ofInt($this->size))
             ->withEntry(Name::of('Root'), $this->root);
+
+        if ($this->info !== null) {
+            $dict = $dict->withEntry(Name::of('Info'), $this->info);
+        }
+
+        if ($this->documentId !== null) {
+            $id = HexString::of($this->documentId);
+            $dict = $dict->withEntry(Name::of('ID'), PdfArray::of($id, $id));
+        }
+
         return "trailer\n" . $dict->toBytes() . "\nstartxref\n" . $this->xrefOffset . "\n%%EOF\n";
     }
 }
--- tests/Unit/Writer/TrailerTest.php
+++ tests/Unit/Writer/TrailerTest.php
@@ -16,4 +16,41 @@ final class TrailerTest extends TestCase
         $expected = "trailer\n<< /Size 4 /Root 1 0 R >>\nstartxref\n250\n%%EOF\n";
         self::assertSame($expected, $trailer->toBytes());
     }
+
+    public function testTrailerWithInfoReference(): void
+    {
+        $trailer = new Trailer(
+            size: 5,
+            root: PdfReference::to(1, 0),
+            xrefOffset: 350,
+            info: PdfReference::to(3, 0),
+        );
+        $expected = "trailer\n<< /Size 5 /Root 1 0 R /Info 3 0 R >>\nstartxref\n350\n%%EOF\n";
+        self::assertSame($expected, $trailer->toBytes());
+    }
+
+    public function testTrailerWithDocumentId(): void
+    {
+        $trailer = new Trailer(
+            size: 4,
+            root: PdfReference::to(1, 0),
+            xrefOffset: 250,
+            documentId: 'abcdef0123456789abcdef0123456789',
+        );
+        $expected = "trailer\n<< /Size 4 /Root 1 0 R /ID [<ABCDEF0123456789ABCDEF0123456789> <ABCDEF0123456789ABCDEF0123456789>] >>\nstartxref\n250\n%%EOF\n";
+        self::assertSame($expected, $trailer->toBytes());
+    }
+
+    public function testTrailerWithInfoAndId(): void
+    {
+        $trailer = new Trailer(
+            size: 6,
+            root: PdfReference::to(1, 0),
+            xrefOffset: 500,
+            info: PdfReference::to(3, 0),
+            documentId: 'abcdef0123456789abcdef0123456789',
+        );
+        self::assertStringContainsString('/Info 3 0 R', $trailer->toBytes());
+        self::assertStringContainsString('/ID [<ABCDEF0123456789ABCDEF0123456789> <ABCDEF0123456789ABCDEF0123456789>]', $trailer->toBytes());
+    }
 }
